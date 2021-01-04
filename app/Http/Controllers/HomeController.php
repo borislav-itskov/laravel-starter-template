cecb691976bf2e04a1ac1ea70fee917812ecd0b7
Borislav Itskov <borislav@devlabs.bg>
2021-01-04 10:41:55 +0200

    remove: homepage used code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\CardService;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,12 +10,10 @@ class HomeController extends Controller
      * The homepage.
      *
      * @method GET
-     * @param  CardService $cardService
      * @return Renderable
      */
-    public function index(CardService $cardService)
+    public function index()
     {
-        $cards = $cardService->findAll();
-        return view('welcome', compact('cards'));
+        return view('welcome');
     }
 }
